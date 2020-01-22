70517705ae205bcdd0b61257b0cfa1af610cd678
yozhik333 <repeinik333@gmail.com>
2020-01-22 18:49:13 +0600

    apoca

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,16 @@
 $(function () {
 
+	$(document).ready(function() {
+		$(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({
+			disableOn: 700,
+			type: "iframe",
+			mainClass: "mfp-fade",
+			removalDelay: 160,
+			preloader: false,
+
+			fixedContentPos: false
+		});
+	});
 	
 	// Owl-Carousel;
 	// $(".owl-carousel").owlCarousel();
@@ -61,3 +72,64 @@ $(function() {
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+
+$('.slider-1').on('initialized.owl.carousel changed.owl.carousel', function (e) {
+	if (!e.namespace) {
+		return;
+	}
+	var carousel = e.relatedTarget;
+	$('.slider-counter').text(carousel.relative(carousel.current()) + 1 + '/' + carousel.items().length);
+}).owlCarousel({
+	loop: true,
+	// margin:10,
+	nav: true,
+	navText: ['<i class="prev">&#8249;</i>', '<i class="next">&#8250;</i>'],
+	responsive: {
+		0: {
+			items: 1
+		},
+		600: {
+			items: 3
+		},
+		1000: {
+			items: 3
+		},
+		1279: {
+			items: 3
+		},
+		1360: {
+			items: 4
+		},
+		1500: {
+			items: 5
+		},
+	}
+});
+
+
+$(document).ready(function () {
+	$('.minus').click(function () {
+		var $input = $(this).parent().find('input');
+		var count = parseInt($input.val()) - 1;
+		count = count < 1 ? 1 : count;
+		$input.val(count);
+		$input.change();
+		return false;
+	});
+	$('.plus').click(function () {
+		var $input = $(this).parent().find('input');
+		$input.val(parseInt($input.val()) + 1);
+		$input.change();
+		return false;
+	});
+});
+
+$('.slider-2').owlCarousel({
+	items: 3,
+	loop: true,
+	margin: 100,
+	nav: true,
+	navText: ['<i class="prev">&#8249;</i>', '<i class="next">&#8250;</i>'],
+	center: true,
+})
